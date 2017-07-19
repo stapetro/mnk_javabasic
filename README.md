86194965889c95eb113740507c728998046fd2ea
Stanislav Petrov <stanislav.i.petrov@gmail.com>
2017-07-19 21:02:11 +0300

    Create project skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+# mnk_designpatterns
+MNKnowledge Java Basics
+
+For details, please see 
+* [MNKnowledge](http://www.mnknowledge.com/courses_en.html "Courses")
+* [MNKnowledge Moodle](https://moodle.mnknowledge.com/ "Requires registration")
---
+++ src/test/java/com/mnknowledge/AppTest.java
@@ -0,0 +1,21 @@
+package com.mnknowledge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+        Assertions.assertThat(true).isTrue();
+    }
+}
